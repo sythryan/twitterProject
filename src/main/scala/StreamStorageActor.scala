3620791fb12267ec5f163e96e86e7b2d9661b2ca
Syth Ryan <syth.ryan@banno.com>
2014-09-11 15:44:33 -0500

    updating top hashtags without any criteria

--- src/main/scala/StreamStorageActor.scala
+++ src/main/scala/StreamStorageActor.scala
@@ -4,8 +4,9 @@ import akka.actor.{Actor, ActorRef}
 import twitter4j._
 import scala.concurrent.future
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Random}
 import java.text.DecimalFormat
+import scala.collection.mutable
 
 // ● Total number of tweets received
 // ● Average tweets per hour/minute/second
@@ -23,16 +24,13 @@ class Extractor(status: Status) {
   val text = status.getText
 }
 
-class StreamStorageActor extends Actor {
-  private[this] var totalTweets = 0
-  private[this] var topEmojis = List("")
-  private[this] var percentEmojis = 0
-  private[this] var topHashtags = List("")
-  private[this] var totalUrlTweets = 0.0
-  private[this] var totalPicTweets = 0.0
-  private[this] var topDomains = List("")
+class TweetProcessingActor(streamStorageActor: ActorRef) extends Actor {
+  def receive = {
+    case e: Status => streamStorageActor ! IncomingTweet(e)
+  }
+}
 
-  val percentFormat = new DecimalFormat("#.00")
+class StreamStorageActor extends Actor {
 
   def receive = {
     case e: IncomingTweet => update(e.status)
@@ -43,6 +41,7 @@ class StreamStorageActor extends Actor {
       "| Average hour/minute/second: " + tweetAvgHour + "/" + tweetAvgMinute + "/" + tweetAvgSecond + "\n" +
       "| Percent Url Tweets: " + percentFormat.format(totalUrlTweets / totalTweets) + "%\n" +
       "| Percent Picture Tweets: " + percentFormat.format(totalPicTweets / totalTweets) + "%\n" +
+      "| Top hashtags: " + topHashtags(0) + ", " + topHashtags(1) + ", " + topHashtags(2) + "\n" +
       "-------------------------------------------------------------------\n"
     ).onComplete {
       case Success(x) => println(x)
@@ -50,16 +49,43 @@ class StreamStorageActor extends Actor {
     }
   }
 
+  val percentFormat = new DecimalFormat("#.00")
+
   val startTime = System.currentTimeMillis / 1000
   def runLength = System.currentTimeMillis / 1000 - startTime
   def tweetAvgSecond = totalTweets / runLength
   def tweetAvgMinute = (totalTweets / (runLength / 60.0)).toInt
   def tweetAvgHour   = (totalTweets / (runLength / 3600.0)).toInt
 
+  private[this] var totalTweets = 0
+  private[this] var topEmojis = List("")
+  private[this] var percentEmojis = 0
+  private[this] val topHashtags = mutable.MutableList("", "", "")
+  private[this] var allHashtagsAndCounts: mutable.Map[String, Int] = mutable.Map()
+  private[this] var totalUrlTweets = 0.0
+  private[this] var totalPicTweets = 0.0
+  private[this] var topDomains = List("")
+
   private[this] def update(status: Status): Unit = {
     totalTweets += 1 
     updateUrlTweets(extractURLs(status))
+    updateHashtags(extractURLs(status))
+  }
 
+  private[this] def updateTopHashtags(hashtag: String): Unit = {
+    topHashtags(Random.nextInt(topHashtags.length)) = hashtag
+  }
+
+  private[this] def updateHashtags(maybeHashtags: List[String]): Unit = {
+    val keys = allHashtagsAndCounts.keys
+    maybeHashtags.foreach{ hashtag =>
+      if (keys.exists(_ == hashtag)) {
+        allHashtagsAndCounts(hashtag) += 1
+      } else {
+        allHashtagsAndCounts += (hashtag -> 1)
+      }
+      updateTopHashtags(hashtag)
+    }
   }
 
   private[this] def updateUrlTweets(urlTweets: List[String]): Unit = {
@@ -76,11 +102,5 @@ class StreamStorageActor extends Actor {
   private[this] def extractHashtags(status: Status): List[String] = {
     val hashtags = status.getHashtagEntities
     hashtags.map(_.getText).toList
-  }
-}
-
-class TweetProcessingActor(streamStorageActor: ActorRef) extends Actor {
-  def receive = {
-    case e: Status => streamStorageActor ! IncomingTweet(e)
   }
 }
